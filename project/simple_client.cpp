affeafeef67b3db1e444cd9b8e48f0b19afe9110
kdrahbar <kdrahbar@asu.edu>
2016-03-20 20:41:18 +0000

    working state thanks to moeiz

--- project/simple_client.cpp
+++ project/simple_client.cpp
@@ -43,15 +43,17 @@ int main(int argc, char *argv[])
         error("ERROR, no such host\n");
         exit(0);
     }
-
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
+	char* addr = "104.197.119.249";
+
+    //bcopy((char *)server->h_addr, 
+    bcopy((char *)addr, 
          (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+         strlen(addr));
+         //server->h_length);
 
     serv_addr.sin_port = htons(portno);
-
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
 
