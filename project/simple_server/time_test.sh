76216d373feeccb65483aa0a36316c89647d7659
kdrahbar <kdrahbar@asu.edu>
2016-03-24 13:29:56 -0400

    added looping through ports

--- project/simple_server/time_test.sh
+++ project/simple_server/time_test.sh
@@ -14,17 +14,24 @@ if [ $1 = "server" ]; then
 fi
 
 if [ $1 = "client" ]; then
+	declare -a ports=("5556" "5557" "5558" "5559" "5560" "5561" "5562" "5563" "5564")
 	file_name="simple_client.cpp"
-	executable="./simpleClient"
+	executable="./simpleClient 5555"
 	executable_name="simpleClient"
 	rm ${executable_name}
 	g++ ${file_name} -o ${executable_name}
 	
 	until $executable; do
-		for ((i=5555;i<=END;i++)); do
-		    ./${executable_name} i
-		    sleep 2
+		for i in "${ports[@]}"
+		do
+		   new_con="$executable_name $i"
+		   ./${new_con}
+		   sleep 2
 		done
+		# for ((i=5555;i<=END;i++)); do
+		#     ./${executable_name} i
+		#     sleep 2
+		# done
 	    sleep 3600
 	done
 fi
