b092b84719ee21f764982debf7909d6996e95870
kdrahbar <kdrahbar@asu.edu>
2016-03-24 17:15:41 -0400

    clean up. latency script working correctly

--- project/simple_server/time_test.sh
+++ project/simple_server/time_test.sh
@@ -11,13 +11,9 @@ if [ $1 = "server" ]; then
 	while true; do
 		./${executable_name}
 	done
-	# until $executable; do
-	#     sleep 1
-	# done
 fi
 
 if [ $1 = "client" ]; then
-	# declare -a ports=("5556" "5557" "5558" "5559" "5560" "5561" "5562" "5563" "5564")
 	file_name="simple_client.cpp"
 	executable="./simpleClient 5555"
 	executable_name="simpleClient"
@@ -26,22 +22,12 @@ if [ $1 = "client" ]; then
 	
 	while true; do
 		
-		# for i in "${ports[@]}"
 		for i in 5556 5557 5558 5559 5560 5561 5562 5563 5564
 		do
 			new_con="$executable_name $i"
 			./${new_con}
-		   # new_con="./$executable_name $i"
-		   # until $new_con; do
-		   #     echo "just finished time to wait"
-		   # done
 		   sleep 5
 		done
 	    sleep 3600
 	done
 fi
-
-# for ((i=5555;i<=END;i++)); do
-		#     ./${executable_name} i
-		#     sleep 2
-		# done
