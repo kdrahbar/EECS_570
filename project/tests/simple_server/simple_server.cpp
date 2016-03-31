086344207fb65a4c5546bbbc6c1da452bbee7c40
kdrahbar <kdrahbar@asu.edu>
2016-03-31 01:10:06 -0400

    added back portnum as cmdline arg

--- project/tests/simple_server/simple_server.cpp
+++ project/tests/simple_server/simple_server.cpp
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     // portno = atoi(argv[1]);
+     portno = atoi(argv[1]);
      // portno = 5555;
 
      serv_addr.sin_family = AF_INET;
