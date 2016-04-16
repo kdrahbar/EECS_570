295056071229fc0c7caa4302954583c95f0c9960
Moeiz Riaz <moeizr@umich.edu>
2016-04-16 05:40:57 +0000

    iperf for diff locations

---
+++ project/tests/max_bandwidth/diff_location/iperf/time_test.sh
@@ -0,0 +1,20 @@
+####
+# End is the end of the range for port nums used
+####
+END=5565
+file_name=""
+executable=""
+
+if [ $1 = "client" ]; then
+		while true; do
+		
+		for i in `seq 1 5`;
+		do
+      echo $i
+			new_con="iperf3 -c 54.152.45.231 -i1 --file 1MB_rand.txt"
+			$new_con >> results.txt
+		   sleep 5
+		done
+	    sleep 3600
+	done
+fi
